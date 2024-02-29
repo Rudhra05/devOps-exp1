9370af3479ca19b2aea49ecc211d599cc88eeb91
Rudhra05 <144410642+Rudhra05@users.noreply.github.com>
2024-02-29 11:19:40 +0530

    Add files via upload

---
+++ demo/src/main/java/com/rudhra/App.java
@@ -0,0 +1,13 @@
+package com.rudhra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
